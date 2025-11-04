bd06905ccfd743c367cf6c375bbe120eb3c60350
hd <home>
2025-11-04 14:58:57 +0000

    Fix Mac build Byte not found

--- Source/ThirdParty/FreeType/src/gzip/ftzconf.h
+++ Source/ThirdParty/FreeType/src/gzip/ftzconf.h
@@ -215,9 +215,8 @@
 #   define FAR
 #endif
 
-#if !defined(MACOS) && !defined(TARGET_OS_MAC)
+// Note, Byte is now required for Mac/iOS too.
 typedef unsigned char  Byte;  /* 8 bits */
-#endif
 typedef unsigned int   uInt;  /* 16 bits or more */
 typedef unsigned long  uLong; /* 32 bits or more */
 
